7bd49049fa14ae4f9fe3dbe9b3820d59e7c9dcd3
Hannes Hochreiner <hannes@hochreiner.net>
2026-07-10 21:11:12 +0200

    fix: guard against nullish fields in expense-list template

--- components/expense-list.js
+++ components/expense-list.js
@@ -33,9 +33,9 @@ export class ExpenseList extends HTMLElement {
       <table>
         ${sorted.map((expense) => /*html*/ `
           <tr data-id="${escapeHtml(expense._id)}">
-            <td>${escapeHtml(expense.date)}</td>
-            <td>${escapeHtml(categoriesById.get(expense.category_id) ?? expense.category_id)}</td>
-            <td class="amount">${expense.amount.toFixed(2)} ${escapeHtml(expense.currency)}</td>
+            <td>${escapeHtml(expense.date ?? "")}</td>
+            <td>${escapeHtml(categoriesById.get(expense.category_id) ?? expense.category_id ?? "")}</td>
+            <td class="amount">${(expense.amount ?? 0).toFixed(2)} ${escapeHtml(expense.currency ?? "")}</td>
             <td class="actions">
               <button class="btn-edit" title="Edit">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="#1f1f1f"><path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z"/></svg>
